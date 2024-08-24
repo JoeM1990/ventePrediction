2d8e432c4b177c54eee1d1f673c85897f7780de4
Joe Monkila <jnkiwa25@gmail.com>
2024-08-24 10:59:34 +0100

    ui index

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("infoModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("infoModal").style.display = "none";
+}
