0c7232fe033e6b5ea6d47bace582c05be7e53fad
Joe Monkila <jnkiwa25@gmail.com>
2024-08-26 16:03:57 +0100

    train and predict

--- scripts/app.js
+++ scripts/app.js
@@ -208,7 +208,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     
-        // document.getElementById('results-section').classList.remove('hidden');
+        document.getElementById('results-section').classList.remove('hidden');
     }
     
     // Génération de recommandations
