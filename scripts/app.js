5dee2397f9e22a136a69864e364f563e823c82da
Joe Monkila <jnkiwa25@gmail.com>
2024-08-27 09:14:55 +0100

    train and predict

--- scripts/app.js
+++ scripts/app.js
@@ -157,13 +157,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 23,444,545,22,12,45,12,55,12,45,
                 23,444,545,22,12
             ]
-
-            const dtes = [
-                '01/01/2024', '01/01/2024', '01/01/2024', '01/01/2024', '02/01/2024', '02/01/2024', '02/01/2024', '02/01/2024', '03/01/2024', '03/01/2024', '03/01/2024', '03/01/2024', '04/01/2024', '04/01/2024', '04/01/2024', '04/01/2024', '05/01/2024', '05/01/2024', '05/01/2024', '05/01/2024', '06/01/2024', '06/01/2024', '06/01/2024', '06/01/2024'
-            ]
         
             predictedValues.forEach((pred, index) => {
-                predictions.push({ date: dtes, sales: sls
+                predictions.push({ date: new Date(data[index].date).toLocaleDateString(), sales: sls
                  });
             });
     
